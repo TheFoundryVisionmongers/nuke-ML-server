d42cdde3e6436eb7e3bd59a6a6fae868c81f4ab5
johannabar <johanna.barbier@eleves.ec-nantes.fr>
2020-05-12 14:17:18 +0100

    Fix typo in comments

--- Models/mrcnn/vis.py
+++ Models/mrcnn/vis.py
@@ -103,7 +103,7 @@ def vis_mask(img, mask, col, alpha=0.4, show_border=True, border_thick=1):
     img[idx[0], idx[1], :] += alpha * col
 
     if show_border:
-        # cv2.findCountours gives (image, contours, hierarchy) back in opencv 3.x
+        # cv2.findContours gives (image, contours, hierarchy) back in opencv 3.x
         # but gives back (contours, hierachy) in opencv 2.x and 4.x
         contours, _ = cv2.findContours(
             mask.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)[-2:]
